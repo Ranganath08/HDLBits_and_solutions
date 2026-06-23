57ca69e6a25adcb6589222c29383b080b44378ca
Ranganath08 <hlranganath2005@gmail.com>
2026-06-23 14:53:24 +0530

    Create One-hit_FSM.v

---
+++ FSM/One-hit_FSM.v
@@ -0,0 +1,8 @@
+// ============================================
+// HDLBits Problem :  FSM
+// Topic           :  One_hit FSM
+// Solved by       :  Ranganath H L
+// Date            :  2026-06-20
+// Link            : https://hdlbits.01xz.net/wiki/Lemmings1
+// ============================================
+
