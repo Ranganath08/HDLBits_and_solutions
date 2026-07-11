3c3864dab5b38680af48f215e6b9c6b4e22598d9
Ranganath08 <hlranganath2005@gmail.com>
2026-07-11 18:05:22 +0530

    Update simple_wire.v

---
+++ Basics/simple_wire.v
@@ -0,0 +1,13 @@
+// ============================================
+// HDLBits Problem :  Basics
+// Topic           :  simple_wire
+// Solved by       :  Ranganath H L
+// Date            :  2026-07-11
+// Link            : https://hdlbits.01xz.net/wiki/Wire
+// ============================================
+
+module top_module( input in, output out );
+    
+    assign out = in;
+
+endmodule
