84f1a8e1c18257a310a2fc19ea46271d629d3dd9
Ranganath08 <hlranganath2005@gmail.com>
2026-07-19 13:00:48 +0530

    Create four_wires.v

---
+++ Basics/four_wires.v
@@ -0,0 +1,18 @@
+// ============================================
+// HDLBits Problem :  Basics
+// Topic           :  four_wire
+// Solved by       :  Ranganath H L
+// Date            :  2026-07-19
+// Link            : https://hdlbits.01xz.net/wiki/Wire4
+// ============================================
+
+module top_module( 
+    input a,b,c,
+    output w,x,y,z );
+
+    assign w = a;
+    assign x = b;
+    assign y = b;
+    assign z = c;
+    
+endmodule
